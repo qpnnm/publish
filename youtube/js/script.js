4225e28963cedbaafd03cc68d0a57e59159e8220
Bik Cha <qpnnm@naver.comm>
2021-01-23 16:25:57 +0900

    20210123

--- youtube/js/script.js
+++ youtube/js/script.js
@@ -1,6 +1,6 @@
 const moreBtn = document.querySelector('.info .moreBtn');
 const title = document.querySelector('.info .title');
-const btnSwitch = document.querySelector('.info .actions li button');
+const btnSwitch = document.querySelector('.info .actions li button .fa-thumbs-up ');
 console.log(btnSwitch);
 moreBtn.addEventListener('click', function() {
     btnSwitch.classList.toggle('clicked');
